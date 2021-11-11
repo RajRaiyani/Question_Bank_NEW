4ac180cb7da0b27570b6184f997a2086812fc18a
R.P.Raiyani <85817080+RajRaiyani@users.noreply.github.com>
2021-11-11 19:46:11 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+let x = 1;
 
+function toggle(){
+    if(x==1){
+        document.getElementById('menu').style.display="block";
+        x=2;
+        return 0;
+    }else{
+        document.getElementById('menu').style.display="none";
+        x=1;
+        return 0;
+    }
+    
+}
